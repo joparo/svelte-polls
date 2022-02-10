4141a920e044d376db42b15ea9ac5e56f85a8caa
Jonas Paro <jonas.paro@omegapoint.se>
2022-02-10 22:42:08 +0100

    First commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+<div class="copyright">Copyright Joparo 2022</div>
+</footer>
+
+<style>
+footer {
+    padding: 40px;
+    text-align: center;
+}
+.copyright {
+    color: #aaa;
+    font-size: 14px;
+    padding: 20px;
+    padding-top: 1px solid #ddd ;
+
+
+}
+</style>
